aa81ae7c29b41e39e27b2959118eb3aed2747a3f
yasar-devops <127598708+yasar-devops@users.noreply.github.com>
2023-03-11 19:43:50 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,7 @@
+echo "# first-java" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yasar-devops/first-java.git
+git push -u origin main
